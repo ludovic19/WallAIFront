905860dcdeeb146cfd34c01a37910a84eb7aae0b
HichamHajla <hichem500@hotmail.com>
2023-02-01 17:42:17 +0100

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,12 +13,53 @@ const Login = () => {
     })
 
   return (
-    <section className="max-w-7xl mx-auto text-center">
+    <section className="max-w-7xl mx-auto ">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Please Log in to your account</h1>
+        <h1 className="font-extrabold text-[#222328] text-center text-[32px]">Please Log in to your account</h1>
       </div>
       <div className="mt-16">
-        <FormField
+      <form>
+      <label>
+       <div>
+       <h2>First Name :</h2>
+       <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+        type="text" name="first_name" onChange={(e) => setLogin((prevState) => ({
+         ...prevState,
+         first_name : e.target.value,
+       }))}/>
+       </div>
+       <div>
+       <h2 className='mt-3'>Last Name :</h2>
+       <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+        type="text" name="last_name" onChange={(e) => setLogin((prevState) => ({
+         ...prevState,
+         last_name : e.target.value,
+       }))}/>
+       </div>
+       <div>
+       <h2 className='mt-3'>Email :</h2>
+       <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3' 
+       type="text" name="email" onChange={(e) => setLogin((prevState) => ({
+         ...prevState,
+         email : e.target.value,
+       }))}/>
+       </div>
+       <div>
+       <h2 className='mt-3'>Password :</h2>
+       <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+        type="text" name="email" onChange={(e) => setLogin((prevState) => ({
+         ...prevState,
+         password : e.target.value,
+       }))}/>
+       </div>
+       <div className='text-center'>
+       <button className="mt-4 text-white bg-[#6469ff] font-medium rounded-md text-xl w-full sm:w-auto px-5 py-2.5 text-center"
+        type="submit" /*onClick={(e) => submit(e)}*/>LOGIN</button>
+       </div>
+     </label>
+     
+   </form>
+        {/* <FormField
           labelName="First Name"
           type="text"
           name="text"
@@ -49,7 +90,7 @@ const Login = () => {
           placeholder=""
           onChange={(e) => setLogin((prevState) => ({...prevState, password : e.target.value,
         }))}
-        />                 
+        />                  */}
       </div>
     </section>
   )
